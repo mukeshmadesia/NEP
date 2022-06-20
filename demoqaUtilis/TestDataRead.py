3b7e9aea3fbff16b30f731709ff9709d9d3a03fc
mukeshmadesia <68299100+mukeshmadesia@users.noreply.github.com>
2022-06-20 22:36:20 +0530

    Update TestDataRead.py

--- demoqaUtilis/TestDataRead.py
+++ demoqaUtilis/TestDataRead.py
@@ -5,66 +5,7 @@ import csv
 
 class TestDataRead:
 
-    @pytest.mark.skip    # Otherwise, pytest will consider it as Testcase
-    @staticmethod
-    def test_data_read():
-        path = '../demoqaTestData/testData.csv'
-
-        os.chdir(os.path.abspath(os.path.dirname(__file__)))
-        # home_path = Path('../Config')
-
-        f = open("../demoqaTestData/testData.csv", "r")
-        data = f.read(100)
-
-        test_username = data.split(";")[0]
-        test_password = data.split(";")[1]
-
-        f.close()
-        return test_username, test_password
-
-    @pytest.mark.skip  # Otherwise, pytest will consider it as Testcase
-    @staticmethod
-    def test_name_read():
-        path = '../demoqaTestData/testData.csv'
-
-        os.chdir(os.path.abspath(os.path.dirname(__file__)))
-        # home_path = Path('../Config')
-
-        f = open("../demoqaTestData/testData.csv", "r")
-        data = f.read(100)
-
-        test_firstname = data.split(";")[2]
-        test_lastname = data.split(";")[3]
-        f.close()
-        return test_firstname, test_lastname
-
-    @pytest.mark.skip  # Otherwise, pytest will consider it as Testcase
-    @staticmethod
-    def test_searchtext_read():
-        path = '../demoqaTestData/testData.csv'
-
-        os.chdir(os.path.abspath(os.path.dirname(__file__)))
-        # home_path = Path('../Config')
-
-        f = open("../demoqaTestData/testData.csv", "r")
-        data = f.read(100)
-
-        test_searchtext = data.split(";")[4]
-
-        f.close()
-        return test_searchtext
-
-    @pytest.mark.skip           # Otherwise, pytest will consider it as Testcase
-    def test_data_write(self, outdata):
-        path = '../demoqaTests/outData.txt'
-
-        os.chdir(os.path.abspath(os.path.dirname(__file__)))
-        # home_path = Path('../Config')
-
-        f = open("outData.txt", "w")
-        data = f.write("name:" + outdata)
-        f.close()
-
+   
     def read_data_from_csv(self, filename):
         # Create an empty List
         datalist = []
